ed3957552d53fed47233f68fc02b37f5605a43b0
Brock Smedley <brocksmedley@gmail.com>
2016-06-22 22:56:48 -0700

    Finds free time; prints to Log>Info

--- GetFreeCalendarSpace/MainActivity.cs
+++ GetFreeCalendarSpace/MainActivity.cs
@@ -31,7 +31,6 @@ namespace GetFreeCalendarSpace {
 												CalendarContract.EventsColumns.Title,
 												CalendarContract.Events.InterfaceConsts.Dtstart,
 												CalendarContract.Events.InterfaceConsts.Dtend
-												
 										   };
 			//list the shit you wanna display
 			string[] sourceColumns = {
@@ -58,24 +57,42 @@ namespace GetFreeCalendarSpace {
 			//new cursor to hold selected events
 			MatrixCursor selectedEventsCursor = new MatrixCursor(calendarsProjection);
 
-			//iterate through items
+			/// find free time between selected events
+			//list to hold times
+			JavaList<FreeTime> freetime = new JavaList<FreeTime>();
+			//start looking for free time at 12AM today; the earliest anything can start today
+			DateTime start = DateTime.Now.ToLocalTime();
+
+			//iterate through items to find selected events
 			while (cursor.MoveToNext()) {
 				//get date for this event (start & end Date/Time in tick format)
 				DateTime sDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(cursor.GetLong(2)).ToLocalTime();//index 2 is start date; 3 is end date
 				DateTime eDate = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(cursor.GetLong(3)).ToLocalTime();
-
 				DateTime tomorrow = DateTime.Today.ToLocalTime().AddDays(1);
 				DateTime now = DateTime.Now.ToLocalTime(); //12AM today
 
-				
 
 				if (sDate >= now && sDate < tomorrow) {
-					Log.WriteLine(Android.Util.LogPriority.Info, "sDate", sDate.ToString());
 					//add item to selected events
-					selectedEventsCursor.AddRow(new Java.Lang.Object[] { cursor.GetString(0), cursor.GetString(1), eDate.ToLocalTime().ToString(), sDate.ToLocalTime().ToString()});
+					selectedEventsCursor.AddRow(new Java.Lang.Object[] { cursor.GetString(0), cursor.GetString(1), eDate.ToString(), sDate.ToString() });
+
+					// if event starts after our starting search point, add free time
+					if (sDate > start) {
+						FreeTime freeTimeChunk = new FreeTime() { Start = start };
+						freeTimeChunk.End = sDate;
+						freetime.Add(freeTimeChunk);
+
+						//reset start for next iteration
+						start = eDate;
+					}	
+				}
+
+				// after last event, add free time from end of event until midnight, as long as event ends before midnight
+				if (cursor.IsLast && eDate < DateTime.Today.AddDays(1)) {
+					freetime.Add(new FreeTime() { Start = start, End = DateTime.Today.AddDays(1) });
 				}
 			}
-			
+
 
 			SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, Resource.Layout.CalListItem, selectedEventsCursor, sourceColumns, targetResources);
 
@@ -83,8 +100,35 @@ namespace GetFreeCalendarSpace {
 			ListView list = FindViewById<ListView>(Resource.Id.listView1);
 			list.Adapter = adapter;
 
-			
+			//list free times
+			for (int i = 0; i < freetime.Size(); i++) {
+				Log.WriteLine(LogPriority.Info, "freetime", "start: " + freetime[i].Start.ToLongDateString() + freetime[i].Start.ToLongTimeString());
+				Log.WriteLine(LogPriority.Info, "freetime", "end: " + freetime[i].End.ToLongDateString() + freetime[i].End.ToLongTimeString());
+			}
 		}
 	}
+
+	// struct to store free times
+	struct FreeTime {
+		private DateTime start;
+		private DateTime end;
+
+		public DateTime Start {
+			get {
+				return start;
+			}
+			set {
+				start = value;
+			}
+		}
+		public DateTime End {
+			get {
+				return end;
+			}
+			set {
+				end = value;
+			}
+		}
+	};
 }
 
